929d095897e60cc6a57bc7acf2ab550f7f12e1f5
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-09-11 03:13:41 +0300

    Added recent version of o2.js and created folder structure

--- o2.js/o2.domhelper.style.js
+++ o2.js/o2.domhelper.style.js
@@ -1,25 +1,8 @@
 /*global window, o2*/
 
 /*
- * Copyright © by Volkan Özçelik - http://o2js.com/
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This program is distributed under the terms of the MIT license.
+ * Please see the LICENSE file for details.
  */
 
 if(!o2.DomHelper) {
@@ -30,6 +13,12 @@ if(!o2.DomHelper) {
  * @module o2.domhelper.style
  * @requires o2
  * @requires o2.stringhelper.transform
+ *
+ * <!--
+ *  This program is distributed under 
+ *  the terms of the MIT license.
+ *  Please see the LICENSE file for details. 
+ * -->
  *
  * <p>A utility package to
  * <strong>add</strong>/<strong>remove</strong>/<strong>modify</strong>
