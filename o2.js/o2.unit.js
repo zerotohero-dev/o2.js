20235da6572a6d183da9ee0ea6f3738b4781ae38
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-09-15 01:53:10 +0300

    unit test demo implementation.

--- o2.js/o2.unit.js
+++ o2.js/o2.unit.js
@@ -1,107 +1,268 @@
-/*global o2*/
+/*
+ * <!--
+ *  This program is distributed under
+ *  the terms of the MIT license.
+ *  Please see the LICENSE file for details.
+ * -->
+ */
+
+/*global o2, alert*/
 ( function(o2, window, UNDEFINED) {
-    //TODO: add header.
+    //TODO: add documentation.
 
     var log = o2.Debugger.log;
+    var assert = o2.Debugger.assert;
 
-    function UnitTest(description, count, testPlan) {
-        this.description = description;
-        this.count = count;
-        this.testPlan = testPlan;
+    var config = {
+        TEST_CHECK_TIMEOUT : 100,
+        TEST_OUTPUT_CONTAINER : 'Output',
+        TEST_SHOULD_USER_CONSOLE : true
+    };
+
+    /**
+     *
+     */
+    function reportGlobalCompletion() {
+
+        // @formatter:off
+        var message = ['All unit tests are complete: ',
+            '(total success: ' , o2.Unit.globalSuccessCount + ', total failure: ' , o2.Unit.globalFailureCount , ')'
+        ].join('');
+        // @formatter:on
+
+        log(message);
+
     }
 
-    UnitTest.prototype.execute = function() {
-        log('executing test: ' + this.description + ' ' + this.count);
-        this.testPlan.apply(this, []);
-    };
-    
-    UnitTest.prototype.hasMoreItems = function(){
-        return this.count > 0;
-    };
-    
-    UnitTest.prototype.didAssertion = function(){
-        this.count--;  
+    /**
+     *
+     */
+    function reportFatalError(unitTest) {
+
+        var message = ['FATAL ERROR in Unit test setup: "', unitTest.description, '"'].join('');
+        unitTest.didAssertion(false, message);
+
+    }
+
+    /**
+     *
+     */
+    function updateTestStatus(unitTest, isSuccess) {
+
+        if(isSuccess) {
+            o2.Unit.globalSuccessCount++;
+            unitTest.successCount++;
+            return;
+        }
+
+        o2.Unit.globalFailureCount++;
+        unitTest.failureCount++;
+    }
+
+    /**
+     *
+     */
+    function updateTestCompletion(unitTest) {
+
+        var isAllSuccess = unitTest.failureCount <= 0;
+
+        // @formatter:off
+        var message = ['Completed: "', unitTest.description, '": ', 
+            '(success:' , unitTest.successCount, ',fail:', unitTest.failureCount, ')'].join('');
+        // @formatter:on
+
+        assert(isAllSuccess, message);
+
+    }
+
+    /**
+     *
+     */
+    function isLocked(activeUnitTest) {
+
+        return activeUnitTest && activeUnitTest.hasMoreItems();
+
+    }
+
+    /**
+     *
+     */
+    function initializeDebugger() {
+
+        try {
+            o2.Debugger.init(config.TEST_OUTPUT_CONTAINER, config.TEST_SHOULD_USE_CONSOLE);
+        } catch(failedToInitializeException) {
+            throw 'o2.Unit.run: Failed to initialize o2.Debugger. No unit tests will be run.';
+        }
+
+    }
+
+    /**
+     *
+     */
+    function UnitTest(description, totalAssertionCount, testPlan) {
+
+        this.description = description;
+        this.remainingCount = totalAssertionCount;
+        this.successCount = 0;
+        this.failureCount = 0;
+        this.testPlan = testPlan;
+
+    }
+
+    var up = UnitTest.prototype;
+
+    /**
+     *
+     */
+    up.execute = function() {
+
+        try {
+            this.testPlan.apply(this, []);
+        } catch(executionException) {
+            this.remainingCount = 0;
+            reportFatalError(this);
+        }
+
     };
 
+    /**
+     *
+     */
+    UnitTest.prototype.hasMoreItems = function() {
 
+        return this.remainingCount > 0;
+
+    };
+
+    /**
+     *
+     */
+    UnitTest.prototype.didAssertion = function(isSuccess, message) {
+
+        assert(isSuccess, message);
+        updateTestStatus(this, isSuccess);
+
+        this.remainingCount--;
+
+        if(this.remainingCount <= 0) {
+            updateTestCompletion(this);
+        }
+
+    };
+
+    /**
+     *
+     */
     o2.Unit = {
+
         globalSuccessCount : 0,
-        globalFailCount : 0,
+        globalFailureCount : 0,
         tests : [],
-        
-        assertEqual : function(unitTest, lValue, rValue, message){
-            log('assserEqual');
-            unitTest.didAssertion();  
+
+        /**
+         *
+         */
+        assert : function(unitTest, expression, message) {
+
+            var result = !!expression;
+            unitTest.didAssertion(result, message);
+
         },
 
+        /**
+         *
+         */
+        assertEqual : function(unitTest, currentValue, expectedValue, message) {
+
+            var result = (currentValue == expectedValue);
+            unitTest.didAssertion(result, message);
+
+        },
+
+        /**
+         *
+         */
+        assertNotEqual : function(unitTest, currentValue, expectedValue, message) {
+
+            var result = (currentValue != expectedValue);
+            unitTest.didAssertion(result, message);
+
+        },
+
+        /**
+         *
+         */
+        assertStrictEqual : function(unitTest, currentValue, expectedValue, message) {
+
+            var result = (currentValue === expectedValue);
+            unitTest.didAssertion(result, message);
+
+        },
+
+        /**
+         *
+         */
+        assertStrictNotEqual : function(unitTest, currentValue, expectedValue, message) {
+
+            var result = (currentValue !== expectedValue);
+            unitTest.didAssertion(result, message);
+
+        },
+
+        /**
+         *
+         */
         pushTest : function(description, testMeta) {
-            var count = testMeta.count;
+
+            var totalAssertionCount = testMeta.count;
             var testPlan = testMeta.test;
-            var unitTest = new UnitTest(description, count, testPlan);
-            o2.Unit.tests.push(unitTest);
+            o2.Unit.tests.push(new UnitTest(description, totalAssertionCount, testPlan));
+
         },
 
         run : function() {
-            o2.Debugger.init('Output', true);
-            
-            log('Started running Unit');
-            
-//            var lock = false;
+
+            initializeDebugger();
+
             var activeUnitTest = null;
- 
-            function isLocked(){
-                return activeUnitTest && activeUnitTest.hasMoreItems();
-            }
-            
-            function unlock(){
-                activeUnitTest = null;
-            }
- 
-            
+
             setTimeout(function waitForUnitTest() {
-                if(isLocked()) {
-                    log('lock...waiting...');
-                    setTimeout(waitForUnitTest, 100);
+
+                if(isLocked(activeUnitTest)) {
+                    setTimeout(waitForUnitTest, config.TEST_CHECK_TIMEOUT);
                     return;
                 }
-
+                //
                 activeUnitTest = o2.Unit.tests.shift();
-                
-                log('got the next unit test');
 
                 if(!activeUnitTest) {
-                    log('released lock... exiting.');
-                    //lock = false;
-                    unlock();
+                    reportGlobalCompletion();
+                    activeUnitTest = null;
                     return;
                 }
 
                 if(!activeUnitTest instanceof UnitTest) {
-                    log('bad instance... exiting.');
-                    //lock = false;
-                    unlock();
+                    activeUnitTest = null;
                     return;
                 }
 
                 if(activeUnitTest.hasMoreItems()) {
-                    log('unit test has more items... locking...');
-                    //lock = true;
                     try {
                         activeUnitTest.execute();
                     } catch(exception) {
 
                     }
-                    //
-                    setTimeout(waitForUnitTest, 100);
+
+                    setTimeout(waitForUnitTest, config.TEST_CHECK_TIMEOUT);
                     return;
                 }
-                log('unit test has no more item... exiting...');
                 //
-                unlock();
+                activeUnitTest = null;
                 return;
-            }, 100);
-            
-            log('Exited function');
+
+            }, config.TEST_CHECK_TIMEOUT);
+
         }
 
     };
--- tests/js/demo.js
+++ tests/js/demo.js
@@ -3,115 +3,54 @@
 
     var Demo = {
         init : function() {
-            o2.Unit.pushTest('o2.$ takes String and retuns DOM node', {
+            o2.Unit.pushTest('parameter exists and it is a number', {
                 count : 2,
                 test : function() {
-//                    debugger;
+                    var me = this;
+
+                    var param = 42;
+
+                    o2.Unit.assert(me, param, 'param exists.');
+
+                    setTimeout(function() {
+
+                        o2.Unit.assertEqual(me, ( typeof param), 'number', 'param is a Number.');
+
+                    }, 2000);
+
+                    //this._will.raise_an_error.thats_the_point = 1;
+
+                }
+
+            });
+
+            o2.Unit.pushTest('parameter exists and it is a String', {
+                count : 2,
+                test : function() {
+                    
                     var me = this;
 
                     var param = 'testItem';
-                    var result = o2.$(param);
 
-                    //{result: true, message: 'param is String'}  --> addResult
-                    o2.Unit.assertEqual(me, (typeof param), 'string', 'param is String');
+                    o2.Unit.assert(me, param, 'param exists.');
 
                     setTimeout(function() {
-                        o2.Unit.assertEqual(me, (typeof param), 'string', 'param is String');
+                        
+                        o2.Unit.assertEqual(me, ( typeof param), 'string', 'param is String.');
+
                     }, 2000);
+
+                    //this._will.raise_an_error.thats_the_point = 1;
 
                 }
 
             });
 
             o2.Unit.run();
-
-           // alert('hello');
         }
 
     };
 
     Demo.init();
 
-    /*function UnitTest(description, items){
-     this.description = description;
-     this.successCount = 0;
-     this.failCount = 0;
-     this.isDone = false;
-     this.log = [];
-     }
-
-     var p = UnitTest.prototype;
-
-     p.execute = function(){
-     var items = this.items;
-     var item = null;
-     var description = '';
-     var assertion = null;
-     var result = false;
-
-     var unit = this;
-
-     setTimeout(function doChunk(){
-     item = items.pop();
-
-     if(items.length === 0) { return; }
-
-     description = item[0];
-     executeTestCase = item[1];
-
-     executeTestCase();
-
-     setTimeout(doChunk, 500);
-     }, 500);
-     }
-
-     p.addResult(mixed){
-     var result = mixed.result;
-     var description = mixed.description;
-
-     o2.Debugger.assert(result, description);
-
-     if(result){
-     this.successCount++;
-     o2.Unit.globalSuccessCount++;
-     }
-
-     this.failureCount++;
-     o2.Unit.globalFailureCount++;
-     }
-
-     p.hasMoreItems = function(){
-     return !this.isDone;
-     }
-
-     p.complete = function(){
-     this.isDone = true;
-     }*/
-    /* test: function(description, items){
-     return new UnitTest(description, items);
-     }  */
-
-    /*
-     execute -->
-     this.testPlan();
-
-     assertEqual : function(){
-     this.count--;
-     }
-
-     //    me.watch();
-     /*setInterval(function(){
-     if(totalItems !== 0) { continue; }
-     me.complete();
-     },100);*/
-    /*test = tests.pop();
-     test.execute();
-
-     var iid = setInterval(function(){
-     if(test.hasMoreItems()) { return; }
-
-     test = test.pop();
-     }, 500);
-
-     */
 }(o2, this));
