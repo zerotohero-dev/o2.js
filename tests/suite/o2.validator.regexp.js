0c2d5905d21d10a808681b133382f1448976a79b
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-11-15 09:54:25 +0200

    minor changes.

--- tests/suite/o2.validator.regexp.js
+++ tests/suite/o2.validator.regexp.js
@@ -28,8 +28,20 @@
          */
         init : function() {
 
-            add('o2.Validator.isEmail SHOULD match a valid e-mail.');
-            add('o2.Validator.isValidUrl SHOULD match a valid url.');
+            add('o2.Validator.isEmail SHOULD match a valid e-mail.', {
+                count: 2,
+                test : function() {
+                    var me = this;
+
+                    var validEmail = 'volkan@o2js.com';
+                    var invalidEmail = 'volkan[at]o2js[dot]com';
+
+                    var resultValid = o2.Validator.isEmail(validEmail);
+                    var resultInvalid = o2.Validator.ieEmail(invalidEmail);
+                }
+            });
+
+            add('o2.Validator.isUrl SHOULD match a valid url.');
             add('o2.Validator.isWhiteSpace SHOULD match whitespace-only strings.');
 
             run(parent && parent.Runner && parent.Runner.processCompletedSuite);
