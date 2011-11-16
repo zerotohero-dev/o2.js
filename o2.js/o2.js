7dca728aea7423e68fb13a8da6b24fccc01cd1cb
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-11-16 09:53:09 +0200

    completed o2.validator.core unit tests.

--- o2.js/o2.js
+++ o2.js/o2.js
@@ -1,7 +1,7 @@
 /**
  * <b>o2.js</b>
  * @project o2.js - a Coherent Solution to Your JavaScript Dilemma ;)
- * @version 0.23.#1321427370#
+ * @version 0.23.#1321429903#
  * @author Volkan Özçelik
  * @description o2.js - a Coherent Solution to Your JavaScript Dilemma ;)
  */
@@ -80,7 +80,7 @@ this.o2 = {};
      *
      * <p>Project build number.</p>
      */
-    me.build = '#1321427370#';
+    me.build = '#1321429903#';
 
     /**
      * @function {static} o2.$
