4725baf7dd5e01aa7d0c79ecabd90d5d87186bf1
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-11-25 10:04:38 +0200

    revision update & documentation.

--- o2.js/o2.js
+++ o2.js/o2.js
@@ -1,7 +1,7 @@
 /**
  * <b>o2.js</b>
  * @project o2.js - a Coherent Solution to Your JavaScript Dilemma ;)
- * @version 0.23.#1322119982#
+ * @version 0.23.#1322208205#
  * @author Volkan Özçelik
  * @description o2.js - a Coherent Solution to Your JavaScript Dilemma ;)
  */
@@ -80,7 +80,7 @@ this.o2 = {};
      *
      * <p>Project build number.</p>
      */
-    me.build = '#1322119982#';
+    me.build = '#1322208205#';
 
     /**
      * @function {static} o2.$
