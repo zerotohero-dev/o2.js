1fe533b9e790d25ca699aea4c56fd047eb637df7
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-12-06 09:16:22 +0200

    moved constants to the top.

--- examples/vcardapp/js/index.js
+++ examples/vcardapp/js/index.js
@@ -8,36 +8,55 @@
 (function(document){
     'use strict';
 
-    function showVCard() {
-        document.getElementById('VCardActivator').style.display = 'none';
-        document.getElementById('VCardContent').style.display = 'block';
-        document.getElementById('VCardContent').innerHTML =
-            ['<h1>Volkan Özçelik</h1>',
-            '<dl>',
-            '<dt>Web</dt>',
-            '<dd><a href="http://o2js.com">o2js.com</a></dd>',
-            '<dt>Email</dt>',
-            '<dd><a href="mailto:volkan@o2js.com">volkan@o2js.com</a></dd>',
-            '<dt>twitter</dt>',
-            '<dd><a href="http://twitter.com/linkibol">@linkibol</a></dd>',
-            '<dt>LinkedIn</dt>',
-            '<dd><a href="http://linkedin.com/in/volkanozcelik"',
-            '>linkedin.com/in/volkanozcelik</a></dd>',
-            '</dl>',
-            '<p class="clear"><a href="/" id="vcard-volkan-close" class="close"',
-            ' title="close"><span>back to home</span></a></p>'].join('');
+    /*
+     * Common constants.
+     */
+    var kNone = 'none';
+    var kBlock = 'block';
 
+    /*
+     * Common elements.
+     */
+    var kActivatorDiv = 'VCardActivator';
+    var kContentDiv = 'VCardContent';
+    var kVCardButton = 'vcard-volkan';
+    var kVCardCloseButton = 'vcard-volkan-close';
+
+    /*
+     * Common HTML
+     */
+    var vCardHtml = [
+        '<h1>Volkan Özçelik</h1>',
+        '<dl>',
+        '<dt>Web</dt>',
+        '<dd><a href="http://o2js.com">o2js.com</a></dd>',
+        '<dt>Email</dt>',
+        '<dd><a href="mailto:volkan@o2js.com">volkan@o2js.com</a></dd>',
+        '<dt>twitter</dt>',
+        '<dd><a href="http://twitter.com/linkibol">@linkibol</a></dd>',
+        '<dt>LinkedIn</dt>',
+        '<dd><a href="http://linkedin.com/in/volkanozcelik"',
+        '>linkedin.com/in/volkanozcelik</a></dd>',
+        '</dl>',
+        '<p class="clear"><a href="/" id="vcard-volkan-close" class="close"',
+        ' title="close"><span>back to home</span></a></p>'
+    ].join('');
+
+    function showVCard() {
+        document.getElementById(kActivatorDiv).style.display = kNone;
+        document.getElementById(kContentDiv).style.display = kBlock;
+        document.getElementById(kContentDiv).innerHTML = vCardHtml
     }
 
     function closeVCard() {
-        document.getElementById('VCardActivator').style.display = 'block';
-        document.getElementById('VCardContent').style.display = 'none';
+        document.getElementById(kActivatorDiv).style.display = kBlock;
+        document.getElementById(kContentDiv).style.display = kNone;
     }
 
-    document.getElementById('vcard-volkan').onclick = function() {
+    document.getElementById(kVCardButton).onclick = function() {
         showVCard();
 
-        document.getElementById('vcard-volkan-close').onclick = function() {
+        document.getElementById(kVCardCloseButton).onclick = function() {
             closeVCard();
             return false;
         };
--- o2.js/o2.js
+++ o2.js/o2.js
@@ -1,7 +1,7 @@
 /**
  * <b>o2.js</b>
  * @project o2.js - a Coherent Solution to Your JavaScript Dilemma ;)
- * @version 0.23.#1323154515#
+ * @version 0.23.#1323155702#
  * @author Volkan Özçelik
  * @description o2.js - a Coherent Solution to Your JavaScript Dilemma ;)
  */
@@ -80,7 +80,7 @@ this.o2 = {};
      *
      * <p>Project build number.</p>
      */
-    me.build = '#1323154515#';
+    me.build = '#1323155702#';
 
     /**
      * @function {static} o2.$
