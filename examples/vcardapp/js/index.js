c5df8bc14ed959645171785bd9c2837c0006ace4
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-12-06 09:01:55 +0200

    implemented the module

--- examples/vcardapp/js/index.js
+++ examples/vcardapp/js/index.js
@@ -5,41 +5,43 @@
  *  Please see the LICENSE file for details.
  * -->
  */
+(function(document){
+    'use strict';
 
-function showVCard() {
-    document.getElementById('VCardActivator').style.display = 'none';
-    document.getElementById('VCardContent').style.display = 'block';
-    document.getElementById('VCardContent').innerHTML = ' \
-        <h1>Volkan Özçelik</h1>\
-        <dl>\
-        <dt>Web</dt>\
-        <dd><a href="http://o2js.com">o2js.com</a></dd>\
-        <dt>Email</dt>\
-        <dd><a href="mailto:volkan@o2js.com">volkan@o2js.com</a></dd>\
-        <dt>twitter</dt>\
-        <dd><a href="http://twitter.com/linkibol">@linkibol</a></dd>\
-        <dt>LinkedIn</dt>\
-        <dd><a href="http://linkedin.com/in/volkanozcelik"\
-            >linkedin.com/in/volkanozcelik</a></dd>\
-        </dl>\
-        <p class="clear"><a href="/" id="vcard-volkan-close" class="close" title="close"\
-            ><span>back to home</span></a></p>';
+    function showVCard() {
+        document.getElementById('VCardActivator').style.display = 'none';
+        document.getElementById('VCardContent').style.display = 'block';
+        document.getElementById('VCardContent').innerHTML =
+            ['<h1>Volkan Özçelik</h1>',
+            '<dl>',
+            '<dt>Web</dt>',
+            '<dd><a href="http://o2js.com">o2js.com</a></dd>',
+            '<dt>Email</dt>',
+            '<dd><a href="mailto:volkan@o2js.com">volkan@o2js.com</a></dd>',
+            '<dt>twitter</dt>',
+            '<dd><a href="http://twitter.com/linkibol">@linkibol</a></dd>',
+            '<dt>LinkedIn</dt>',
+            '<dd><a href="http://linkedin.com/in/volkanozcelik"',
+            '>linkedin.com/in/volkanozcelik</a></dd>',
+            '</dl>',
+            '<p class="clear"><a href="/" id="vcard-volkan-close" class="close"',
+            ' title="close"><span>back to home</span></a></p>'].join('');
 
-}
-
-function closeVCard() {
-    document.getElementById('VCardActivator').style.display = 'block';
-    document.getElementById('VCardContent').style.display = 'none';
-}
-
-document.getElementById('vcard-volkan').onclick = function() {
-    showVCard();
-
-    document.getElementById('vcard-volkan-close').onclick = function() {
-        closeVCard();
-        return false;
     }
-    return false;
-}
 
+    function closeVCard() {
+        document.getElementById('VCardActivator').style.display = 'block';
+        document.getElementById('VCardContent').style.display = 'none';
+    }
 
+    document.getElementById('vcard-volkan').onclick = function() {
+        showVCard();
+
+        document.getElementById('vcard-volkan-close').onclick = function() {
+            closeVCard();
+            return false;
+        };
+
+        return false;
+    };
+}(this.document));
