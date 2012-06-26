eaa0583a9d7d1c4852d2ebd7ba3f090444f6dd80
Jose Capablanca <jose@o2js.com>
2012-06-26 00:14:03 -0700

    #o2js created a node.js package for the npm registry.

--- batch/publish.sh
+++ batch/publish.sh
@@ -9,7 +9,7 @@
 # | Please see the LICENSE file for details. |
 # +------------------------------------------+
 #
-# lastModified: 2012-05-27 11:24:12.738049
+# lastModified: 2012-06-25 22:37:43.356927
 #
 php update_build_number.php
 ant -f o2.xml
@@ -17,9 +17,12 @@ ant -f o2.xml
 #rm -rf /var/www/html/o2.js
 rm -rf /Applications/MAMP/htdocs/o2.js
 
-#TOOD: put this in a commonly-editabl configuration file.
+# TODO: put this in a commonly-editabl configuration file.
 #rsync -rv --exclude=.git ../ /var/www/o2.js/
 rsync -r --exclude=.git ../ /Applications/MAMP/htdocs/o2.js/
+
+# Export as a NodeJS Module
+sh nodejs_export.sh
 
 # ALl done:
 echo 'All Done!'
