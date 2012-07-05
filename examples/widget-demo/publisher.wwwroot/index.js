df6e8ea47c1fcf2d212cd003c2d404f6697860b2
Volkan Ozcelik <volkan.ozcelik@gmail.com>
2012-07-05 16:53:14 +0300

    #o2js added initial draft for the blogpost.

---
+++ examples/widget-demo/publisher.wwwroot/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+console.log('hello');
+
+var app = express.createServer();
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/publisher/home', function(req, res) {
+    res.send([
+
+        '<!doctype html>',
+        '<html>',
+        '<head>',
+        '</head>',
+        '<body>',
+        '<h2>Welcome to Publisher Website</h2>',
+        '</body>',
+        '</html>'
+
+    ].join(''));
+});
+
+app.listen(80);
