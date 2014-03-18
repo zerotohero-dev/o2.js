5f36762aa5e19903c21ec1aba584b7e9e08d9205
Volkan Ozcelik <volkan@o2js.com>
2014-03-18 08:26:56 -0700

    implementing a promise...

---
+++ src/o2/then/future/core.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var isFunction = require('../validation/core').isFunction,
+    identity = require('../functional/core').identity;
+
+/**
+ * @method Future
+ * @constructor
+ *
+ * @param deferred
+ * @param onFulfilled
+ * @param onRejected
+ */
+function Future(deferred, onFulfilled, onRejected) {
+    this.deferred = deferred;
+    this.onFulfilled = onFulfilled;
+    this.onRejected = onRejected;
+}
+
+/**
+ * @method resolve
+ * @final
+ *
+ * @param value
+ */
+Future.prototype.resolve = function(value) {
+    handle(this.deferred, isFunction(this.onFulfilled) ? this.onFulfilled : identity, value);
+};
+
+/**
+ * @method reject
+ * @final
+ *
+ * @param reason
+ */
+Future.prototype.reject = function(reason) {
+    handle(this.deferred, isFunction(this.onRejected) ? this.onRejected : identity, reason);
+};
+
+module.exports = Future;
