bc427252c2d4bcf343b76ba2f1c3ef45bcf396b0
Volkan Ozcelik <volkan@o2js.com>
2014-07-26 20:25:38 -0700

    implemented a very basic async logger.
    And also did dome minor refactoring.

--- amd/o2/debug/core.js
+++ amd/o2/debug/core.js
@@ -7,19 +7,73 @@ define(function (require, exports, module) {'use strict';
  * Please see the LICENSE.md file for details.
  */
 
-var isConsoleAvailable = (typeof console !== 'undefined');
+var isNode = (typeof module !== 'undefined' && !!module.exports),
+    isConsoleAvailable = (typeof console !== 'undefined'),
 
-if (isConsoleAvailable) {
+    kFs = 'fs',
+
+    stream;
+
+function print(label, args) {
+    var buffer = [],
+        i, len;
+
+    for (i = 0, len = args.length; i < len; i++) {
+        buffer.push(args[i]);
+    }
+
+    stream.write(label + ': ' + buffer.join(','));
+}
+
+if (isNode) {
+    exports.initialize = function(file) {
+
+        // To trick grunt-contrib-jasmine.
+        var fs = require(kFs);
+
+        if (typeof file === 'string') {
+            stream = fs.createFileStream(
+                file, {flags: 'a+', encoding: 'utf8'}
+            );
+
+            return;
+        }
+
+        stream = file;
+    };
+
     exports.log = function() {
-        console.log.apply(console, arguments);
+        if (!stream) {
+            throw new Error(
+                'o2.debug: Please call `o2.debug.initialize` first.'
+            );
+        }
+
+        print('[LOG ]', arguments);
     };
 
     exports.warn = function() {
-        console.warn.apply(console, arguments);
+        if (!stream) {
+            throw new Error(
+                'o2.debug: Please call `o2.debug.initialize` first.'
+            );
+        }
+
+        print('[WARN]', arguments);
     };
 } else {
-    exports.log = function() {};
-    exports.warn = function() {};
+    if (isConsoleAvailable) {
+        exports.log = function() {
+            console.log.apply(console, arguments);
+        };
+
+        exports.warn = function() {
+            console.warn.apply(console, arguments);
+        };
+    } else {
+        exports.log = function() {};
+        exports.warn = function() {};
+    }
 }
 
 });
